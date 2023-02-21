226ebd3dc8eafa61f459eb10e85daa0463b5979a
Orange <lb16673514910@163.com>
2023-02-21 18:18:55 +0800

    同步

---
+++ laobeiblog-server/src/test/java/com/xi/LaobeiblogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaobeiblogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

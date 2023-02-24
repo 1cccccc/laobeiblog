cb9e43fdab5abe8644e0267a16489287cc7a4991
1cccccc <68212098+1cccccc@users.noreply.github.com>
2023-02-24 08:50:03 +0800

    Delete laobeiblog-web directory

--- laobeiblog-web/src/plugins/mdEditor/previehtml.js
+++
@@ -1,7 +0,0 @@
-import VMdPreviewHtml from '@kangc/v-md-editor/lib/preview-html';
-import '@kangc/v-md-editor/lib/style/base-editor.css';
-
-// 引入使用主题的样式
-import '@kangc/v-md-editor/lib/theme/style/github.css';
-
-export default VMdPreviewHtml;
